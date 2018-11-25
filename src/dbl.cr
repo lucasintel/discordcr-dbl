840861875300427321be1b1c80f4292c1e77e3f3
kandayo <kandayo@riseup.net>
2018-11-25 14:05:08 -0300

    Update shard name

--- src/dbl.cr
+++
@@ -1,8 +0,0 @@
-require "./dbl/mappings.cr"
-require "./dbl/exceptions.cr"
-require "./dbl/client.cr"
-require "./dbl/server.cr"
-
-module Dbl
-  VERSION = "0.1.0"
-end
---
+++ src/discordcr-dbl.cr
@@ -0,0 +1,8 @@
+require "./discordcr-dbl/mappings.cr"
+require "./discordcr-dbl/exceptions.cr"
+require "./discordcr-dbl/client.cr"
+require "./discordcr-dbl/server.cr"
+
+module Dbl
+  VERSION = "0.1.0"
+end
